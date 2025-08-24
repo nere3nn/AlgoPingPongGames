3f4c2a09c61751a6c74e45e7ca63aa6951a35507
nere3nn <rakhajaya8@gmail.com>
2025-08-24 11:07:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -34,19 +34,35 @@ class GameSprite(sprite.Sprite):
 #main player class
 class Player(GameSprite):
    #method to control the sprite with arrow keys
-    def update(self):
+    def update_racket_2(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
 
-racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
-racket2 = Player('racket.png', 520, 200, 4, 50, 150)
-ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+    def update_racket_1(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
 
+
+racket1 = Player('racket.png', 30, 200, 30, 100, 5)
+racket2 = Player('racket.png', 520, 200, 30, 100, 5)
+ball = GameSprite('tennis_ball.png', 270, 230, 50, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose_1 = font.render('PLAYER LOSE..', True, (180, 0, 0))
+lose_2 = font.render('PLAYER LOSE..', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
 
 run = True
+finish = False
 clock = time.Clock()
 fps = 60
 """ program utama """
@@ -55,10 +71,28 @@ while run:
         if e.type == QUIT:
             run = False
 
+    if finish != True:
+        window.fill(background_color)
+
+        racket1.update_racket_1()
+        racket2.update_racket_2()
+
+
+        #ball move
+        ball.rect.x +=  speed_x
+        ball.rect.y +=  speed_y
+
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+        speed_y = speed_y * -1
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
     racket1.reset()
     racket2.reset()
     ball.reset()
 
-    window.fill(background_color)
+    
     display.update()
     clock.tick(fps)
